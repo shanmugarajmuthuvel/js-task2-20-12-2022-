9734fca509cb2c9d350f492a7ef35184c5f5f4f1
shanmugarajmuthuvel <shanmugarajmuthuvel1777@yahoo.com>
2022-12-20 12:15:44 +0530

    array method from

---
+++ test2.js
@@ -0,0 +1,12 @@
+ var food=[]
+     var a=prompt("Enter the list of Food")
+	 for(i=0; i<a; i++){
+		    food[i]=prompt("Enter Your Food")
+	 }
+	 document.write(food)
+	 document.write("<br>")
+	 var chf=prompt("Enter the change food")
+	 for(i=chf;i<=chf; i++){
+		 food[i]=prompt("Enter your Food")
+	 }
+	 document.write(food)
